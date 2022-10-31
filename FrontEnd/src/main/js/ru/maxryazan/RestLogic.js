a7c627661af02c937782c5cf3c0b68199ab69e7b
Максим Олегович <no4ta87@mail.ru>
2022-10-31 21:17:20 +0300

    v 1.1 stars added

--- FrontEnd/src/main/js/ru/maxryazan/RestLogic.js
+++ FrontEnd/src/main/js/ru/maxryazan/RestLogic.js
@@ -1,4 +1,3 @@
-const shops__list = document.querySelector('.shops__list');
 
 
 fetch('http://localhost:8080/flowers/api/v1/flower_shops', {
@@ -24,16 +23,37 @@ function printShops(object){
     object.forEach( (obj) => {
         document.querySelector('.containerInlineLeft').insertAdjacentHTML('afterbegin',
             `
-            <h1>${obj.shopTitle}</h1>
-            <h2>Адрес: ${obj.location}</h2>
-            <h2>Оценка:<color class="star"> ${rebuildGrade(obj.grade)}</color></h2>
-            <h2>Время открытия: ${obj.openTime}</h2>
-            <h2>Время закрытия: ${obj.closingTime}</h2>
+            <div class="inline">
+                <div class="oneShopContainer">
+                    <h1>${obj.shopTitle}</h1>
+                    <h2>Адрес: ${obj.location}</h2>
+                    <h2>Оценка:<color class="star"> ${rebuildGrade(obj.grade)}</color></h2>
+                    <h2>Время открытия: ${obj.openTime}</h2>
+                    <h2>Время закрытия: ${obj.closingTime}</h2>
+                </div>
+                <div class="imagesContainer">
+                    <h1>${obj.shopTitle}</h1>
+                </div>
+            </div>    
             `
         )
     }
 )
 }
+
+window.addEventListener('resize', () => {
+    let mainWindow = document.querySelector('.containerInlineMain')
+    if(window.innerWidth < 990){
+        mainWindow.style.width = `${window.innerWidth - 150}px`
+    }
+    if(window.innerWidth < 760){
+        mainWindow.style.width = `${window.innerWidth - 50}px`
+    }
+})
+
+
+
+
 
 // "http://localhost:8080/flowers/api/v1/{title}"
 // "http://localhost:8080/flowers/api/v1/flower_shops"

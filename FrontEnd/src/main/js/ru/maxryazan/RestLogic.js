7be5bde60a7fb788106c8c184db0559abd002447
Максим Олегович <no4ta87@mail.ru>
2022-11-06 18:56:28 +0300

    v 1.7 search was modify, @media screens was added

--- FrontEnd/src/main/js/ru/maxryazan/RestLogic.js
+++ FrontEnd/src/main/js/ru/maxryazan/RestLogic.js
@@ -1,6 +1,6 @@
 let count = 1;
 const URL_getAll = 'http://localhost:8080/flowers/api/v1/flower_shops';
-let allShops = []
+let width = 400
 
 document.querySelector('.searchInput').addEventListener('focus', () =>{
     document.querySelector('.searchInput').style.outline ='0'
@@ -10,6 +10,7 @@ function search() {
     let userInput = document.querySelector('.searchInput').value.toLowerCase().trim();
     let titles = [...document.querySelectorAll('.shopTitle')];
     let hiders = document.querySelectorAll('.class-for-hide')
+
     for(let item of titles){
         if(item.innerHTML.toLowerCase().indexOf(userInput) !== -1){
             hiders.forEach(elem => elem.classList.add('hide'))
@@ -20,12 +21,12 @@ function search() {
     }
 }
 
-
-document.querySelector('.searchButton').addEventListener('click', search)
+document.querySelector('.searchInput').addEventListener('keypress', search)
 
 fetch(URL_getAll, { headers: { "Content-type": "application/json; charset=UTF-8" }})
         .then((res) => res.json())
         .then((shops) => printShops(shops));
+
 
 function rebuildGrade(grade){
     switch (grade.toUpperCase()) {
@@ -39,7 +40,6 @@ function rebuildGrade(grade){
 }
 
 function printShops(object){
-    allShops.push(object)
     object.forEach( (obj) => {
         let length = obj.imagePath.length
         document.querySelector('.containerInlineMain').insertAdjacentHTML('afterbegin',
@@ -59,9 +59,9 @@ function printShops(object){
                     onclick="relocation(document.getElementById('${obj.shopTitle.replace(" ", "") + count}'), ${length})">
                 </div>    
                 <div class="sliderLine">    
-                    <div class="inline2" id="${obj.shopTitle.replace(" ", "") + count}">    
-                        ${obj.imagePath.map(image => `<img src="../images/${image}" alt="" class="img">`).join("")}
-                    </div>    
+                    <div class="inline2" id="${obj.shopTitle.replace(" ", "") + count}">  
+                         ${obj.imagePath.map(image => `<img src="../images/${image}" alt="" class="img">`).join("")}
+                     </div>    
                 </div>    
             </div>                  
            `)
@@ -69,59 +69,15 @@ function printShops(object){
 }
 
 function relocation(element, length) {
-    // let width =  document.querySelector('.img').clientWidth;
-    let width = 400
+    if(window.innerWidth > 1990){
+        width = 700
+    }
     element.style.transform = 'translate(-'+ width * count+'px)'
-    console.log(document.querySelector('.img').clientWidth)
     count++
     if(count === length) {
         count=0
     }
 }
-
-    window.addEventListener('resize', () => {
-        let mainWindow = document.querySelector('.containerInlineMain')
-        let searchInput = document.querySelector('.searchInput')
-
-        if (window.innerWidth < 990) {
-                mainWindow.style.width = `${window.innerWidth}px`
-        } else {
-            if (window.innerWidth > 2160) {
-                mainWindow.style.width = `${window.innerWidth / 2.5}px`
-            } else {
-                if (window.innerWidth < 1500) {
-                    mainWindow.style.width = `${window.innerWidth / 1.5}px`
-                }
-            }
-        }
-        if (window.innerWidth > 2160) {
-            mainWindow.style.marginLeft = "10%"
-            mainWindow.style.marginTop = "17%"
-            mainWindow.style.transform = "scale(1.5)"
-            searchInput.style.width = '400px'
-            searchInput.style.height = '50px'
-            searchInput.style.fontSize = '28px'
-        } else {
-            if (window.innerWidth < 2160) {
-                mainWindow.style.marginLeft = "0"
-                mainWindow.style.marginTop = "0"
-                mainWindow.style.transform = "scale(1)"
-                searchInput.style.width = '200px'
-                searchInput.style.height = '24px'
-                searchInput.style.fontSize = '18px'
-            }
-        }
-        if (window.innerWidth > 2360) {
-            mainWindow.style.marginLeft = "10%"
-            mainWindow.style.marginTop = "13%"
-            searchInput.style.width = '500px'
-            searchInput.style.height = '70px'
-            searchInput.style.fontSize = '35px'
-        }
-    })
-
-
-
 
 
 
